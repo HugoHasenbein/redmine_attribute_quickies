4ad2b2b8e54bafdc578a422b927b0daed884acfc
Dr. Stephan Wenzel <stephan.wenzel@drwpatent.de>
2018-07-13 20:03:27 +0200

    added theme compatibility

--- lib/attribute_quickies/hooks/attribute_quickies_context_menu.rb
+++
@@ -1,6 +0,0 @@
-module AttributeQuickiesContextMenu
-  class Hooks < Redmine::Hook::ViewListener
-    render_on :view_issues_context_menu_start, :partial => 'hooks/attribute_quickies/context_menu_head'
-  end
-end
-
--- lib/attribute_quickies/hooks/attribute_quickies_css.rb
+++
@@ -1,6 +0,0 @@
-module AttributeQuickiesCSS
-  class Hooks < Redmine::Hook::ViewListener
-	render_on :view_layouts_base_html_head, :partial => 'hooks/attribute_quickies/style_sheet'
-  end
-end
-
